1d75ed9b33165d30809d780f0bbf4ca45df6d8d7
Jiajie Chen <c@jia.je>
2022-07-05 21:03:39 +0800

    Initial commit

---
+++ poly.py
@@ -0,0 +1,9 @@
+# TODO: read n and x from standard input
+# HINT: use input()
+
+# TODO: read n+1 integers from standard input
+# TODO: compute polynomial
+# HINT: for loop
+
+# TODO: print the result
+# HINT: use print()
