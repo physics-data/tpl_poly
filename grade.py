b5d8be50e36119daf4cc205e16bab7ec107ae02a
Harry Chen <i@harrychen.xyz>
2022-07-06 16:02:42 +0800

    Update grader
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- grade.py
+++ grade.py
@@ -24,7 +24,7 @@ if sys.version_info[0] != 3:
 
 def file_lines(file):
     lines = []
-    with open(file, 'r') as fd:
+    with open(file, 'r', encoding='utf-8') as fd:
         for line in fd:
             line = line.strip()
             if len(line) > 0:
@@ -38,6 +38,8 @@ if __name__ == '__main__':
     data = dict()
     files = [f for f in os.listdir(DATA_PATH) if isfile(join(DATA_PATH, f)) and f.startswith('in')]
 
+    success_count = 0
+
     for file in sorted(files):
         file_in = join(DATA_PATH, file)
         num = int(file[2:-4])
@@ -47,6 +49,10 @@ if __name__ == '__main__':
             os.remove(file_out)
         except FileNotFoundError:
             pass
+
+        message = []
+        success = True
+
         p = subprocess.Popen([sys.executable, PROGRAM], stdin=open(
             file_in, 'r'), stdout=open(file_out, 'w'), stderr=subprocess.PIPE)
         start_time = time.time()
@@ -54,20 +60,39 @@ if __name__ == '__main__':
         while p.poll() is None:
             if time.time() - start_time > TIME_LIMIT:
                 p.kill()
+                message.append('Time limit exceeded')
+                success = False
+        else:
+            if not os.path.isfile(file_out):
+                message.append('No output file found')
+                success = False
 
-        ans, out = file_lines(file_ans), file_lines(file_out)
-        if ans == out:
-            grade += FULL_SCORE / len(files)
-        elif os.isatty(1):
-            print('Data %d: expected %s, but got %s' %
-                (num, repr(ans), repr(out)))
-            stdout, stderr = p.communicate(timeout=1)
-            if len(stderr) > 0:
-                print('       : your program exited with:')
-                sys.stdout.buffer.write(stderr)
+        if success:
+            ans, out = file_lines(file_ans), file_lines(file_out)
+            if len(ans) != len(out):
+                    message.append(f'Line count mismatch: should be {len(ans)}, got {len(out)}')
+                    success = False
+            else:
+                for i in range(len(ans)):
+                    if out[i] != ans[i]:
+                        message.append(f'Line {i} mismatch: should be \'{ans[i]}\', get \'{out[i]}\'')
+                        success = False
+                        break
+
+        if success:
+            success_count += 1
+            if os.isatty(1):
+                print(f'Testcase {num}: PASS')
+        else:
+            if os.isatty(1):
+                stdout, stderr = p.communicate(timeout=1)
+                if len(stderr) > 0:
+                    message.append(f'Your program exited with: {stderr.decode("utf-8")}')
+                print(f'Testcase {num}: {", ".join(message)}')
+
 
     # output grade
-    grade = int(grade)
+    grade = int(FULL_SCORE / len(files) * success_count)
     data['grade'] = grade
     if os.isatty(1):
         print(f'Grade: {grade}/{int(FULL_SCORE)}')
